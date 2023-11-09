5d0ba29b8d415becb3987ad2d7d695b06a59aab8
Jonathan Coulon <jonathan.coulon@gmail.com>
2023-11-09 01:17:44 +0100

    Code Refactoring

--- src/test/java/com/chatop/chatopapi/ChatopApiApplicationTests.java
+++ src/test/java/com/chatop/chatopapi/ChatopApiApplicationTests.java
@@ -1,13 +1,33 @@
 package com.chatop.chatopapi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-class ChatopApiApplicationTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.chatop.chatopapi.controller.RentalController;
+import com.chatop.chatopapi.service.RentalService;
+
+@WebMvcTest(controllers = RentalController.class)
+class RentalControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private RentalService rentalService;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testGetRentals() throws Exception {
+		mockMvc.perform(get("/api/rentals")).andExpect(status().isOk());
 	}
 
 }
